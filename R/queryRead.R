b1609a4b2cb97806af5b2d9fb2f06598e5b24218
Paul Meinz <paulmeinz@gmail.com>
2017-08-29 16:30:15 -0700

    ADDED DOCUMENTATION FOR QUERY READ

--- R/queryRead.R
+++ R/queryRead.R
@@ -1,18 +1,12 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-#
+#' Read a query or text document in as on character value
+#'
+#'@param fileName the directory or name of a text file.
+#'
+#'@return A character value
+#'
+#'@examples
+#'\dontrun{queryRead('exampleQuery.txt')}
+#'@export
 
 queryRead <- function(fileName) {
   query <- readChar(fileName, file.info(fileName)$size)
